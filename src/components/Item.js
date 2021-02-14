92a2103392c444ea9ae1d70c4263a0de7a2999b3
JFiedler23 <43707498+JFiedler23@users.noreply.github.com>
2021-02-14 16:40:25 -0600

    Updated item cards

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,9 +2,14 @@
 function Item(props){
     return (
         <div className="item" id={props.rarity}>
-            <p className="item-name">{props.name}</p>
             <img className="item-image" src={props.image} alt={props.name}></img>
-            <p className="item-price">{props.price}</p>
+            <div className="item-content">
+                <span className="item-name">{props.name}</span>
+                <span className="item-price">
+                    <img className="price-icon" src="https://image.fnbr.co/price/icon_vbucks_50x.png"></img>
+                    {props.price}
+                </span>
+            </div>
         </div>
     )
 }
